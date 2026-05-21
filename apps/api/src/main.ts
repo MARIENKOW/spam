09000e3ae063e8d023acddb669161a775258b3ae
Denis Marienkow <den.marienkow@gmail.com>
2026-05-21 04:31:30 +0300

    fix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from "@/modules/app/app.module";
 import { NestFactory } from "@nestjs/core";
 import cookieParser from "cookie-parser";
 
+
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix(env.NEXT_PUBLIC_API_GLOBAL_PREFIX);
